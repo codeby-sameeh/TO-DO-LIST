23080bae78cb0653c88206614af3d303d917e6a2
muhammed sameeh <mhdsameeh8@gmail.com>
2024-07-28 10:00:38 +0530

    third-commmit

--- todo.js
+++ todo.js
@@ -3,9 +3,15 @@ let input = document.getElementById('inputfield')
 let output = document.getElementById('Outputfield')
 let button = document.getElementById('add-a-button')
 let completetask = document.getElementById('cmplt-task')
+let darkmode = document.getElementById('dark-mode')
 
 
 function display(){
+    if(input.value== ""){
+        alert('type somethig')
+    }
+    else{
+           
    const newli = document.createElement('li')
    const edit = document.createElement('button')
     const dlt = document.createElement('button')
@@ -21,17 +27,29 @@ function display(){
     dlt.style.width = '20px'
     edit.textContent ="edit"
     newli.appendChild(edit)
-
+   input.value = ""
 
     
     check.addEventListener('click', function() {
         completetask.appendChild(newli)
+        check.remove()
+        dlt.remove()
+        edit.remove()
     });
    dlt.addEventListener('click',function(){
         newli.remove()
-   })
-    }  
+    })
+    edit.addEventListener('click',function(){
+        newli.value = input.value
+    })
+   }}
     
+  
+  darkmode.addEventListener('click', function(){
+    document.body.classList.toggle('dark-mode')
+  })
+
+
   
     
    
